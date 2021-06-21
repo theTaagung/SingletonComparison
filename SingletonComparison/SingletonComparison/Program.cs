16ec483b15a569a9f697e3f8bc881382e0ec338e
Atul Sharma <sharma.atul.kr@gmail.com>
2021-06-20 22:33:24 -0500

    Add files via upload

---
+++ SingletonComparison/SingletonComparison/Program.cs
@@ -0,0 +1,50 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Diagnostics.Windows.Configs;
+using BenchmarkDotNet.Running;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonComparison
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BenchmarkRunner.Run<ComparePerfromance>();
+            Console.ReadLine();
+        }
+    }
+
+    [KeepBenchmarkFiles]
+    [RPlotExporter]
+    [HtmlExporter]
+    [NativeMemoryProfiler]
+    [MemoryDiagnoser]
+    public class ComparePerfromance
+    {
+        [Benchmark]
+        public void CallNormal()
+        {
+           
+            for (int i = 0; i < 5; i++)
+            {
+                DAO ObjDAO = new DAO();
+                int iCount = ObjDAO.GetStates().Count;
+            }
+        }
+
+        [Benchmark]
+        public void CallSingleton()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                int iCount = StateDataCache.Names.Count;
+            }
+
+        }
+
+    }
+}
---
+++ SingletonComparison/SingletonComparison/StateDataCache.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonComparison
+{
+    public sealed class StateDataCache
+
+    {
+        private static Lazy<StateDataCache> local = new Lazy<StateDataCache>(GetData);
+
+        private List<string> _names = null;
+
+        public static List<string> Names
+        {
+            get
+            {
+                return local.Value._names;
+            }
+        }
+        private static StateDataCache GetData()
+        {
+            DAO ObjDAO = new DAO();
+            StateDataCache cache = new StateDataCache();
+            cache._names = ObjDAO.GetStates();
+            Console.WriteLine("GetData Method Called");
+            return cache;
+        }
+
+
+    }
+}
