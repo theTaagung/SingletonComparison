738563f212b5b548cb0843dcaa9eb8d6c3bae0f0
Atul Sharma <sharma.atul.kr@gmail.com>
2021-06-20 22:26:35 -0500

    Add files via upload

---
+++ SingletonComparison/SingletonComparison/DAO.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonComparison
+{
+    public class DAO
+    {
+        public List<string> GetStates()
+        {
+            List<string> states = new List<string>();
+
+            var sts = File.ReadAllLines(@"C:\Hands-On\SingletonComparison\SingletonComparison\Data\States.txt");
+            foreach (var item in sts)
+            {
+                states.Add(item);
+            }
+
+            return states;
+        }
+    }
+}
